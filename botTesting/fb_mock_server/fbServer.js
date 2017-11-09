096c0d73944889168a6b46dcfa9e1008b2ff8150
shankarkumarc <shankarkumar.chaudhary@alacriti.com>
2017-11-09 20:28:43 +0530

    running test1

--- botTesting/fb_mock_server/fbServer.js
+++ botTesting/fb_mock_server/fbServer.js
@@ -1,13 +1,14 @@
 var nock = require('nock')
 var appConf = require("../../conf/appconf")
 var webhookUrl = appConf.webhookUrl
+console.log("Mocking FB server")
 
 nock(appConf.fbUrl)
-.post('/')
-.reply(200,messageFromWebhook)
+.post('?access_token='+appConf.token,messageFromWebhook)
+.reply(200,{})
 
-function messageFromWebhook(argument) {
-    console.log(argument)
+function messageFromWebhook(body) {
+    console.log("in messageFromWebhook",body)
     return {
         
     }
--- botTesting/test.js
+++ botTesting/test.js
@@ -4,3 +4,5 @@ var appserver = require('../src/webapp/app')
 // callTest
 console.log("fbTest")
 var fbTest = require('./fb_test/fbTest');
+
+var fbServer = require('./fb_mock_server/fbServer')
